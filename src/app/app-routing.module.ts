dbf7822bb6732c335243e70627d59154ac69de6d
akshaylandage-rhealtech <akshay.landage@rhealtech.com>
2022-04-27 20:54:35 +0530

    edit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BooksComponent } from './Components/books/books.component';
+import { InsertEditDeleteComponent } from './Components/insert-edit-delete/insert-edit-delete.component';
 
 const routes: Routes = [
-{ path:'booklist', component: BooksComponent }
-
+  { path: 'booklist', component: BooksComponent },
+  { path: 'insert/:elementId', component: InsertEditDeleteComponent },
+  { path: 'edit', component: InsertEditDeleteComponent },
+  { path: 'delete', component: InsertEditDeleteComponent }
 ];
 
 @NgModule({
